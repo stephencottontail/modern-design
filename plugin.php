8531cdcb8e30c102fa34f358b19282029366dd4f
Stephen Dickinson <stephencottontail@me.com>
2019-02-03 10:31:43 -0700

    Add cookies modal

--- plugin.php
+++ plugin.php
@@ -14,3 +14,14 @@ add_action( 'wp_enqueue_scripts', function() {
 
     wp_enqueue_script( 'sc-modern-script', plugins_url( 'dist/modern-design.js', __FILE__ ), array( 'jquery' ), null, true );
 } );
+
+add_action( 'wp_footer', function() { ?>
+   <div class="sc-modal sc-cookies">
+       <img class="sc-modal__image" src="<?php echo plugins_url( 'img/flower.png', __FILE__ ); ?>" />
+       <p><strong>Attention!</strong> This website doesn't just use cookies, it dispatches invisible drones to watch you and your loved ones. If you'd like to learn more about the technology behind these invisible drones, click "Learn More". Otherwise, click "I Understand" to demonstrate that you really don't care whether invisible drones stalk you and your loved ones.</p>
+       <div class="sc-cookies__buttons">
+           <button class="sc-button sc-button__primary sc-button__primary--blue" id="accept-cookies">I Accept!</button>
+           <button class="sc-button sc-button__secondary sc-button__secondary--blue" id="reject-cookies">Learn More</button>
+        </div>
+    </div>
+<?php }, 50 );
