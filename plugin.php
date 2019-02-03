e70badc360289a9179495eda61c08ab350c7f068
Stephen Dickinson <stephencottontail@me.com>
2019-02-03 12:39:10 -0700

    Add app and newsletter modals

--- plugin.php
+++ plugin.php
@@ -15,17 +15,6 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_script( 'sc-modern-script', plugins_url( 'dist/modern-design.js', __FILE__ ), array( 'jquery' ), null, true );
 } );
 
-add_action( 'wp_footer', function() { ?>
-    <div class="sc-modal sc-cookies">
-        <img class="sc-modal__image" src="<?php echo plugins_url( 'img/flower.png', __FILE__ ); ?>" />
-        <p><strong>Attention!</strong> This website doesn't just use cookies, it dispatches invisible drones to watch you and your loved ones. If you'd like to learn more about the technology behind these invisible drones, click "Learn More". Otherwise, click "I Understand" to demonstrate that you really don't care whether invisible drones stalk you and your loved ones.</p>
-        <div class="sc-cookies__buttons">
-            <button class="sc-button sc-button__primary sc-button__primary--blue" id="accept-cookies">I Accept!</button>
-            <button class="sc-button sc-button__secondary sc-button__secondary--blue" id="reject-cookies">Learn More</button>
-        </div>
-    </div>
-<?php }, 50 );
-
 add_action( 'wp_head', function() {
     $phrases = array(
         'Home',
@@ -49,3 +38,45 @@ add_action( 'wp_head', function() {
     echo '</div>';
     echo '</div>';
 }, 50 );
+
+add_action( 'wp_footer', function() { ?>
+    <div class="sc-modal sc-cookies">
+        <img class="sc-modal__image" src="<?php echo plugins_url( 'img/flower.png', __FILE__ ); ?>" />
+        <p><strong>Attention!</strong> This website doesn't just use cookies, it dispatches invisible drones to watch you and your loved ones. If you'd like to learn more about the technology behind these invisible drones, click "Learn More". Otherwise, click "I Understand" to demonstrate that you really don't care whether invisible drones stalk you and your loved ones.</p>
+        <div class="sc-cookies__buttons">
+            <button class="sc-button sc-button__primary sc-button__primary--blue" id="accept-cookies">I Accept!</button>
+            <button class="sc-button sc-button__secondary sc-button__secondary--blue" id="reject-cookies">Learn More</button>
+        </div>
+    </div>
+<?php }, 50 );
+
+add_action( 'wp_footer', function() { ?>
+    <div class="sc-modal__wrapper">
+        <div class="sc-modal sc-mobile-app">
+            <span class="sc-modal__title">Learn About Our Mobile App?</span>
+            <p><strong>Hey!</strong> We now have a mobile app. It's been scientifically proven that using our mobile app instead of our website makes you 18.756% less likely to get scurvy.</p>
+            <div class="sc-mobile-app-buttons">
+                <button class="sc-button sc-button__primary sc-button__primary--blue" id="accept-mobile-app"><strong>Yes!</strong> I enjoy mobile apps.</button>
+                <button class="sc-button sc-button__secondary sc-button__secondary--blue" id="reject-mobile-app"><strong>No</strong>, I want to contract scurvy.</button>
+            </div>
+        </div>
+    </div>
+<?php }, 60 );
+
+add_action( 'wp_footer', function() { ?>
+    <div class="sc-modal__wrapper">
+        <div class="sc-modal sc-newsletter">
+            <span class="sc-modal__title">Sign Up For Our Newsletter</span>
+            <img class="sc-modal__image" src="<?php echo plugins_url( 'img/landscape.png', __FILE__ ); ?>" />
+            <p><strong>Good news!</strong> We now have a newsletter. Please commit to receiving email notifications about new content before you even get a chance to check us out. And as a bonus, we'll sell your email address to a bunch of dodgy companies so you can receive hundreds of useless emails per week!</p>
+            <form>
+                <label for="sc-email">Email:</label>
+                <input type="email" name="sc-email" />
+            </form>
+            <div class="sc-newsletter-buttons">
+                <button class="sc-button sc-button__primary sc-button__primary--blue" id="accept-newsletter"><strong>Yes</strong>, sign me up!</button>
+                <button class="sc-button sc-button__secondary sc-button__secondary--blue" id="reject-newsletter">X</button>
+            </div>
+        </div>
+    </div>
+<?php }, 70 );
